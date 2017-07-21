8d3285d3d04c0c684bf92d1ffd7e4eb032f312d2
wanghao8080 <117663444@qq.com>
2017-07-21 15:07:33 +0800

    bcrypt: 加 salt 的 hash

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-const crypto = require('crypto')
-const hash = crypto.createHash('sha256')
+const bcrypt = require('bcrypt')
 
-hash.update('password')
-console.log(hash.digest('base64'))
+const password = 'password'
+
+bcrypt.genSalt(10, (error, salt) => {
+  console.log('salt: ', salt)
+
+  bcrypt.hash(password, salt, (error, hash) => {
+    console.log('hash: ', hash)
+  })
+})
+
+const hashPassword = '$2a$10$R83yg2xqFHHikRbp1mzf9.NwLk7dOqGL1WEre74kt/Y9YUxFxiFfW'
+const userInputPassword = 'password1'
+
+bcrypt.compare(userInputPassword, hashPassword)
+  .then(result => console.log(result))
