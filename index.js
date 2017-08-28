f1f17709890c084fefa1ed01a75f1f9409411ce4
wanghao8080 <117663444@qq.com>
2017-08-28 21:21:10 +0800

    文件上传过滤器

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+
+const fileFilter = (request, file, callback) => {
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+    return callback(new Error('images only :)'), false)
+  }
+  callback(null, true)
+}
+
+const upload = multer({ dest: 'uploads/', fileFilter })
 
 app.post('/profile', upload.single('avatar'), (request, response, next) => {
   response.send(request.file)
@@ -9,6 +17,12 @@ app.post('/profile', upload.single('avatar'), (request, response, next) => {
 
 app.post('/photos/upload', upload.array('photos', 3), (request, response, next) => {
   response.send(request.files)
+})
+
+app.use((error, request, response, next) => {
+  response.status(500).send({
+    message: error.message
+  })
 })
 
 app.listen(8080, () => {
